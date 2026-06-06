79ab353f6dad411607fcd36733d66269b12a4a3d
xiangyouma <414570186@m365.fju.edu.tw>
2026-06-06 15:16:37 +0800

    [AI-USED]Move Dockerfile next to pom.xml

--- 414570186_馬祥祐_Ch09C/miniclinic/src/main/java/tw/edu/fju/miniclinic/model/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package tw.edu.fju.miniclinic.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PatientRepository extends JpaRepository<Patient, String> {
-
-    List<Patient> findByName(String name);
-}
